2304ef0fefc5249c549e6687e2deb0489889b524
Kim ju hwan <minecraft_bos@naver.com>
2023-12-28 10:02:44 +0900

    🧑‍💻:: Baekjoon 9550
    백준 9550번 문제

---
+++ 9550.py
@@ -0,0 +1,10 @@
+T = int(input())
+print(T//8)
+# human = 0
+# for i in range(T):
+#     N, K = map(int, input().split())
+#     candy = list(map(int, input().split()))
+#     for j in range(N):
+#         human += candy[j] // K
+#     print(human)
+        
