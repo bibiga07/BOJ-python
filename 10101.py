01e2614d906b0cb1068ef40bcb3b5f3c9307b6e0
Kim ju hwan <minecraft_bos@naver.com>
2024-03-04 23:10:16 +0900

    🧑‍💻:: Baekjoon 10101
    백준 10101번 문제

---
+++ 10101.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a+b+c != 180:
+    print("Error")
+elif a == 60 and b == 60 and c == 60:
+    print("Equilateral")
+elif ((a == b or a == c) or (b == c)):
+    print("Isosceles")
+elif ((a != b and a != c) and (b != c)):
+    print("Scalene")
